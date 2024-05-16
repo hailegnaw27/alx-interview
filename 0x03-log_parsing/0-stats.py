b243c0e6ec102e87b90cbd2ef80fc9604d52e307
hailegnaw27 <ixvos406@gmail.com>
2024-05-16 15:02:30 -0700

    interview

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,40 +1,37 @@
 #!/usr/bin/python3
+'''Script for parsing log files.'''
+
 import sys
 
-def parse_line(line):
-    # Parse the input line according to the specified format
-    # Return IP Address, status code, and file size
+cache = {'200': 0, '301': 0, '400': 0, '401': 0,
+         '403': 0, '404': 0, '405': 0, '500': 0}
+total_size = 0
+counter = 0
+
+try:
+    for line in sys.stdin:
+        line_list = line.split(" ")
+        if len(line_list) > 4:
+            code = line_list[-2]
+            size = int(line_list[-1])
+            if code in cache.keys():
+                cache[code] += 1
+            total_size += size
+            counter += 1
+
+        if counter == 10:
+            counter = 0
+            print('File size: {}'.format(total_size))
+            for key, value in sorted(cache.items()):
+                if value != 0:
+                    print('{}: {}'.format(key, value))
+
+except Exception as err:
     pass
 
-def print_statistics(total_size, status_codes):
-    # Print statistics: total file size and number of occurrences of each status code
-    pass
-
-def main():
-    total_size = 0
-    status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-    lines_read = 0
-
-    try:
-        for line in sys.stdin:
-            # Parse the line
-            ip_address, status_code, file_size = parse_line(line.strip())
-
-            # Update metrics
-            total_size += file_size
-            if status_code in status_codes:
-                status_codes[status_code] += 1
-
-            lines_read += 1
-
-            # Print statistics after every 10 lines
-            if lines_read % 10 == 0:
-                print_statistics(total_size, status_codes)
-    except KeyboardInterrupt:
-        # Handle keyboard interruption gracefully
-        print_statistics(total_size, status_codes)
-        sys.exit(0)
-
-if __name__ == "__main__":
-    main()
+finally:
+    print('File size: {}'.format(total_size))
+    for key, value in sorted(cache.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value)))
 

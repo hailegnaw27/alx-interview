c8937eaf15fdddff564c52522d33955da63ec3a0
hailegnaw27 <ixvos406@gmail.com>
2024-05-16 15:16:04 -0700

    final

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,30 +1,67 @@
 #!/usr/bin/python3
 
+import re
 import sys
 
-def parse_line(line):
-    # Implement parsing logic for each line here
-    pass
+def extract_input(input_line):
+    patterns = (
+        r'\s*(?P<ip>\S+)\s*',
+        r'\s*\[(?P<date>\d+\-\d+\-\d+ \d+:\d+:\d+\.\d+)\]',
+        r'\s*"(?P<request>[^"]*)"\s*',
+        r'\s*(?P<status_code>\S+)',
+        r'\s*(?P<file_size>\d+)'
+    )
+    log_format = '{}\\-{}{}{}{}\\s*'.format(*patterns)
+    match = re.fullmatch(log_format, input_line)
+    info = {
+        'status_code': 0,
+        'file_size': 0,
+    }
+    if match is not None:
+        info['status_code'] = match.group('status_code')
+        info['file_size'] = int(match.group('file_size'))
+    return info
 
-def print_statistics(total_size, status_code_counts):
-    # Implement logic to print statistics here
-    pass
+def print_statistics(total_file_size, status_codes_stats):
+    print('Total file size: {:d}'.format(total_file_size))
+    for status_code in sorted(status_codes_stats.keys()):
+        count = status_codes_stats.get(status_code, 0)
+        if count > 0:
+            print('{:s}: {:d}'.format(status_code, count))
 
-def main():
-    total_size = 0
-    status_code_counts = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-    
+def update_metrics(line, total_file_size, status_codes_stats):
+    line_info = extract_input(line)
+    status_code = line_info.get('status_code', '0')
+    if status_code in status_codes_stats.keys():
+        status_codes_stats[status_code] += 1
+    return total_file_size + line_info['file_size']
+
+def run():
+    line_num = 0
+    total_file_size = 0
+    status_codes_stats = {
+        '200': 0,
+        '301': 0,
+        '400': 0,
+        '401': 0,
+        '403': 0,
+        '404': 0,
+        '405': 0,
+        '500': 0,
+    }
     try:
         for line in sys.stdin:
-            # Process each line using the parse_line function
-            # Update total_size and status_code_counts accordingly
-            
-            # Check if 10 lines have been processed and print statistics
-            # Use the print_statistics function
-            
-    except KeyboardInterrupt:
-        # Handle keyboard interruption and print final statistics
+            total_file_size = update_metrics(
+                line.strip(),
+                total_file_size,
+                status_codes_stats,
+            )
+            line_num += 1
+            if line_num % 10 == 0:
+                print_statistics(total_file_size, status_codes_stats)
+    except (KeyboardInterrupt, EOFError):
+        print_statistics(total_file_size, status_codes_stats)
 
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    run()
 

906a474730756f553789b572d8d568fb1b2eadc5
hailegnaw27 <ixvos406@gmail.com>
2024-05-26 08:16:28 -0700

    0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -6,33 +6,22 @@ UTF-8 Validation Module
 
 def validUTF8(data):
     """
-    Validate if the data set is a valid UTF-8 encoding.
-
-    :param data: List of integers representing bytes o data
-    :return: True if data is valid.
+    gets the data and checks if its correct utf8
     """
-    num_bytes = 0
+    byte_count = 0
 
-    for byte in data:
-        if byte < 0 or byte > 255:
-            return False
-
-        binary_repr = format(byte, '#010b')[-8:]
-
-        if num_bytes == 0:
-            if binary_repr.startswith('0'):
-                continue
-            elif binary_repr.startswith('110'):
-                num_bytes = 1
-            elif binary_repr.startswith('1110'):
-                num_bytes = 2
-            elif binary_repr.startswith('11110'):
-                num_bytes = 3
-            else:
+    for i in data:
+        if byte_count == 0:
+            if i >> 5 == 0b110 or i >> 5 == 0b1110:
+                byte_count = 1
+            elif i >> 4 == 0b1110:
+                byte_count = 2
+            elif i >> 3 == 0b11110:
+                byte_count = 3
+            elif i >> 7 == 0b1:
                 return False
         else:
-            if not binary_repr.startswith('10'):
+            if i >> 6 != 0b10:
                 return False
-            num_bytes -= 1
-
-    return num_bytes == 0
+            byte_count -= 1
+    return byte_count == 0

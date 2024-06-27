3c641fe13d4d358c26c326685ede19f95cc128fa
hailegnaw27 <ixvos406@gmail.com>
2024-06-27 08:09:40 -0700

    0-island_perimeter.py

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -3,11 +3,12 @@
 0-island_perimeter.py
 """
 
+
 def island_perimeter(grid):
     """
     Calculates the perimeter of the island described in the grid.
 
-    :param grid: List of list of integers (0 represents water, 1 represents land)
+    :param grid: List of list of integers (0 represents water)
     :return: The perimeter of the island
     """
     perimeter = 0
@@ -17,16 +18,19 @@ def island_perimeter(grid):
             if grid[row][col] == 1:
                 # Start with a perimeter of 4 for the current land cell
                 perimeter += 4
-                
+
                 # Check the cell above (if it exists and is land)
                 if row > 0 and grid[row - 1][col] == 1:
-                    perimeter -= 2  # Subtract 2 (1 for this cell and 1 for the adjacent cell)
+                    # Subtract 2 (1 for this cell and 1 for the adjacent cell)
+                    perimeter -= 2
 
                 # Check the cell to the left (if it exists and is land)
                 if col > 0 and grid[row][col - 1] == 1:
-                    perimeter -= 2  # Subtract 2 (1 for this cell and 1 for the adjacent cell)
+                    # Subtract 2 (1 for this cell and 1 for the adjacent cell)
+                    perimeter -= 2
 
     return perimeter
+
 
 if __name__ == "__main__":
     grid = [
@@ -37,4 +41,3 @@ if __name__ == "__main__":
         [0, 0, 0, 0, 0, 0]
     ]
     print(island_perimeter(grid))
-
